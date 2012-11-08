2c0e8723c70c9fb40f5bc8fab57c6294c0e4d4e6
Alan O'Donnell <alan.m.odonnell@gmail.com>
2012-11-08 13:01:10 -0500

    Improve ebc_bitfield

--- src/ebc_bitfield.erl
+++ src/ebc_bitfield.erl
@@ -1,24 +1,34 @@
 -module(ebc_bitfield).
 -include_lib("eunit/include/eunit.hrl").
 
--export([add/2]).
+-export([at/2,
+         add/2,
+         contains/2]).
 
-add(Index, Bitfield) when Index >= byte_size(Bitfield) * 8 ->
-    MiddleSize = Index - byte_size(Bitfield),
-    <<Bitfield/bitstring, 0:MiddleSize, 1:1>>;
-add(0, Bitfield) ->
-    <<_:1, Right/bitstring>> = Bitfield,
-    <<1:1, Right/bitstring>>;
+at(Index, Bitfield) ->
+    <<_L:Index/bitstring, Bit:1, _R/bitstring>> = Bitfield,
+    Bit.
+
 add(Index, Bitfield) ->
     <<Left:Index/bitstring, _:1, Right/bitstring>> = Bitfield,
     <<Left/bitstring, 1:1, Right/bitstring>>.
+
+contains(Index, Bitfield) ->
+    at(Index, Bitfield) == 1.
 
 %% ------------------------------------------------------------------
 %% EUnit tests
 %% ------------------------------------------------------------------
 
 add_test() ->
-    ?assertEqual(<<1:1>>, add(0, <<0:1>>)),
     ?assertEqual(<<128>>, add(0, <<0>>)),
-    ?assertEqual(<<255>>, add(7, <<254>>)),
-    ?assertEqual(<<0, 1>>, add(15, <<>>)).
+    ?assertEqual(<<64>>, add(1, <<0>>)),
+    ?assertEqual(<<0, 128>>, add(8, <<0, 0>>)).
+
+contains_test() ->
+    ?assert(contains(0, <<128>>)),
+    ?assertNot(contains(1, <<128>>)),
+    ?assert(contains(1, <<64>>)),
+    ?assertNot(contains(0, <<64>>)),
+    ?assert(contains(8, <<0, 128>>)),
+    ?assertNot(contains(9, <<0, 128>>)).
