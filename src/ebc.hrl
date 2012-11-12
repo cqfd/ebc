ab1aac44ec44eceb2aa6d504c9f565bdd626a570
Alan O'Donnell <alan.m.odonnell@gmail.com>
2012-11-12 09:35:21 -0500

    Clean up type defs in ebc.hrl

--- src/ebc.hrl
+++ src/ebc.hrl
@@ -1,10 +1,26 @@
--type bittorrent_msg() :: {'handshake', Resrvd :: binary(), InfoHash :: binary(), PeerId :: binary()}
-                        | 'keep_alive' | 'choke' | 'unchoke' | 'interested' | 'not_interested'
-                        | {'have', PieceIndex :: integer()}
-                        | {'bitfield', Bitfield :: binary()}
-                        | {'request', Index :: integer(), Begin :: integer(), Length :: integer()}
-                        | {'piece', Index :: integer(), Begin :: integer(), Block :: binary()}
-                        | {'cancel', Index :: integer(), Begin :: integer(), Length :: integer()}.
+-type bittorrent_msg() :: {'handshake',
+                           Resrvd :: binary(),
+                           InfoHash :: binary(),
+                           PeerId :: binary()}
+                        | 'keep_alive'
+                        | 'choke'
+                        | 'unchoke'
+                        | 'interested'
+                        | 'not_interested'
+                        | {'have',
+                           PieceIndex :: integer()}
+                        | {'bitfield',
+                           Bitfield :: binary()}
+                        | {'request',
+                           Index :: integer(),
+                           Begin :: integer(),
+                           Length :: integer()}
+                        | {'piece',
+                           Index :: integer(),
+                           Begin :: integer(),
+                           Block :: binary()}
+                        | {'cancel',
+                           Index :: integer(),
+                           Begin :: integer(),
+                           Length :: integer()}.
 
--type send_bittorrent_msg() :: {'send', bittorrent_msg()}.
--type recv_bittorrent_msg() :: {'recv', bittorrent_msg()}.
