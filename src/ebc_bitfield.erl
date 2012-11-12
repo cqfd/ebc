e8b44043fcb09af41476b876bbb4ba58ad36f761
Alan O'Donnell <alan.m.odonnell@gmail.com>
2012-11-12 08:53:25 -0500

    Chop off unused ebc_bitfield api

--- src/ebc_bitfield.erl
+++ src/ebc_bitfield.erl
@@ -1,20 +1,12 @@
 -module(ebc_bitfield).
 -include_lib("eunit/include/eunit.hrl").
 
--export([at/2,
-         add/2,
-         contains/2]).
-
-at(Index, Bitfield) ->
-    <<_L:Index/bitstring, Bit:1, _R/bitstring>> = Bitfield,
-    Bit.
+-export([add/2,
+         difference/2]).
 
 add(Index, Bitfield) ->
     <<Left:Index/bitstring, _:1, Right/bitstring>> = Bitfield,
     <<Left/bitstring, 1:1, Right/bitstring>>.
-
-contains(Index, Bitfield) ->
-    at(Index, Bitfield) == 1.
 
 difference(As, Bs) ->
     difference(As, Bs, 0, []).
@@ -38,14 +30,6 @@ add_test() ->
     ?assertEqual(<<128>>, add(0, <<0>>)),
     ?assertEqual(<<64>>, add(1, <<0>>)),
     ?assertEqual(<<0, 128>>, add(8, <<0, 0>>)).
-
-contains_test() ->
-    ?assert(contains(0, <<128>>)),
-    ?assertNot(contains(1, <<128>>)),
-    ?assert(contains(1, <<64>>)),
-    ?assertNot(contains(0, <<64>>)),
-    ?assert(contains(8, <<0, 128>>)),
-    ?assertNot(contains(9, <<0, 128>>)).
 
 difference_test() ->
     ?assertEqual([7], difference(<<1>>, <<0>>)),
